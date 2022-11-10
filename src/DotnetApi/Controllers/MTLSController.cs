3f1ce84aaf748970e13658e3e380beb1d6abf18e
William Espinosa <william.espinosa@lambda3.com.br>
2022-11-10 16:42:28 -0300

    update controller

--- src/DotnetApi/Controllers/MTLSController.cs
+++ src/DotnetApi/Controllers/MTLSController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography.X509Certificates;
+using System.IO;
 
 namespace DotnetApi.Controllers;
 
@@ -17,11 +18,14 @@ public class MTLSController : ControllerBase
     [HttpPost()]
     public async Task<ActionResult> GetAsync([FromBody]Requisicao requisicao)
     {
-        var certificado = X509Certificate2.CreateFromPemFile(requisicao.Certificado, requisicao.Chave);
+        var pathCertificado = Path.Combine("certs", requisicao.Certificado);
+        var pathChave = Path.Combine("certs", requisicao.Chave);
+
+        var certificado = X509Certificate2.CreateFromPemFile(pathCertificado, pathChave);
         var handler = new HttpClientHandler();
         handler.ClientCertificateOptions = ClientCertificateOption.Manual;
         handler.ClientCertificates.Add(certificado);
-        //handler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+        handler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
         var client = new HttpClient(handler);
         var resultado = await client.GetAsync(requisicao.Url);
         var conteudo = await resultado.Content.ReadAsStringAsync();
